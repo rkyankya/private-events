03f639ac03a72a349a87f021e8b665a1b79e46bb
rkyankya <rkyankya@gmail.com>
2020-02-12 19:51:53 +0300

    Added Events model:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
