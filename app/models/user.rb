c91c3116a7953eb4c48a2ae223a3937c547e5f65
lucasmazo32 <lucasmazo32@gmail.com>
2020-02-12 18:24:48 -0500

    Fixed minor bugs with the databases

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: 'creator', class_name: 'Event',
                     dependent: :destroy
-  has_many :attendances, foreign_key: 'attendee', class_name: 'Invitation'
-  has_many :attended_events, through: :attendances
+  has_many :invitations, foreign_key: 'attendee', class_name: 'Invitation'
+  has_many :attended_events, through: :invitations
   has_many :permisions, foreign_key: 'allow', class_name: 'Invitation'
-  has_many :invitations, through: :permisions
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
