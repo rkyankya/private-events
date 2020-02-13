010779d8e15994a28cb57795dc5b436ad2ecb344
lucasmazo32 <lucasmazo32@gmail.com>
2020-02-13 15:53:55 -0500

    made advanced step in the odin project

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,8 +1,29 @@
+# frozen_string_literal: true
+
 class InvitationsController < ApplicationController
   def new; end
 
   def create
-    # params[:invitation]
-    redirect_to root_path
+    re_path = request.original_fullpath
+    check_inv = Invitation.where(attended_event_id: params[:event_id])
+    if params[:invitation]
+      params[:invitation].each do |invit|
+        check = check_inv.find_by(attendee_id: invit[0].to_i)
+        if invit[1] == '1'
+          unless check
+            Invitation.create(attended_event_id: params[:event_id],
+                              attendee_id: invit[0].to_i)
+          end
+        else
+          check&.delete
+        end
+      end
+      flash[:success] = 'Invitations sent!'
+    else
+      check = check_inv.find_by(attendee_id: current_user.id)
+      check.accepted = !check.accepted
+      check.save
+    end
+    redirect_to re_path
   end
 end
--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -14,7 +14,10 @@ module InvitationsHelper
 
   def going?(user_id)
     inv = Invitation.where(attended_event_id: params[:id])
-    inv.find_by(attendee_id: user_id).first.accepted == true
+    inv = inv.find_by(attendee_id: user_id)
+    return if inv.nil?
+
+    inv.accepted == true
   end
 
   def invited?(user_id)
@@ -28,5 +31,19 @@ module InvitationsHelper
     return false if inv.first.nil?
 
     inv.map(&:attended_event)
+  end
+
+  def events_going(user_id)
+    inv = Invitation.where(attendee_id: user_id)
+    inv = inv.where(accepted: true)
+    inv.map(&:attended_event)
+  end
+
+  def go_nogo
+    inv = Invitation.where(attended_event_id: params[:id])
+    inv = inv.find_by(attendee_id: current_user.id)
+    return "I've changed my mind" if inv.accepted
+
+    "I'm going"
   end
 end
